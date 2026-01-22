76894e6793e619fc0fc2557ac5705318077bcbd7
Dheeraj Srivastava <dheerajn2311@gmail.com>
2026-01-22 15:57:33 +0530

    Enhance fetchData function with comments
    Added comments explaining the async and await keywords.

--- asyncawait.js
+++ asyncawait.js
@@ -1,4 +1,7 @@
-    async function fetchData(){
+// asyn -Declearing a function with async make it return promise.
+// await - It paused the execution of code until the promise its waiting for resolve then the return the resolve value      
+
+async function fetchData(){
 
         try{
             const response = await fetch("https://jsonplaceholder.typicode.com/");
